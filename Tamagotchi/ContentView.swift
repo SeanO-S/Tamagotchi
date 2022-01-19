385bb08ab282d7f84abff86f1b99a073fdd4bbcf
Sean Okuyama-Smith <okuyama-smith.s@etoncollege.org.uk>
2022-01-19 11:21:16 +0000

    UI stuff

--- Tamagotchi/ContentView.swift
+++ Tamagotchi/ContentView.swift
@@ -9,8 +9,43 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("ååååå")
-            .padding()
+        VStack{
+            ZStack{
+                Image("Blue-Egg-W")
+                    .resizable()
+                    .frame(width: 300, height: 400, alignment: .center)
+                VStack{
+                    Rectangle()
+                        .frame(width: 180, height: 180, alignment: .center)
+                        .padding(.top, 40)
+                    HStack{
+                        Button(action: {}){
+                            Circle()
+                                .background(Color.white)
+                                .foregroundColor(.white)
+                                .frame(width: 20, height: 20, alignment: .center)
+                                .clipShape(Circle())
+                        }
+                        Button(action: {}){
+                            Circle()
+                                .background(Color.white)
+                                .foregroundColor(.white)
+                                .frame(width: 20, height: 20, alignment: .center)
+                                .clipShape(Circle())
+                                .padding(.top, 20)
+                        }
+                        Button(action: {}){
+                            Circle()
+                                .background(Color.white)
+                                .foregroundColor(.white)
+                                .frame(width: 20, height: 20, alignment: .center)
+                                .clipShape(Circle())
+                        }
+                            
+                    }//End of H
+                }//End of V
+            }//End of Z
+        }//End of V
     }
 }
 
